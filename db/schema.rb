5faa167199594e5c8a3d153e0ee07d33d11b3757
Kamal K Garg <kamalkgarg@gmail.com>
2016-05-28 07:52:17 +0530

    Crated posts table by executing 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528021628) do
+ActiveRecord::Schema.define(version: 20160528022040) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
